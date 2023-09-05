4503a9a46425fa0d8a86dcfef82010d8803045dd
Vikram Shanker <v1shanker@gmail.com>
2023-09-05 10:48:12 -0400

    header + dropdown

--- seating-chart/src/App.js
+++ seating-chart/src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 
 import * as React from 'react';
+import { CssVarsProvider, extendTheme } from '@mui/joy/styles';
 import Autocomplete from '@mui/joy/Autocomplete';
 import Typography from '@mui/joy/Typography';
 
@@ -10,21 +11,49 @@ const name_table_map = [
   { name: 'Name 2', id: 2 },
 ];
 
+const titletheme =  extendTheme({
+  typography: {
+    fontFamily: 'Serathine',
+  },
+  titleheader: {
+    flexGrow: 1,
+    alignItems: 'center',
+    background: "#000000",
+    typography: {
+      fontFamily: 'Serathine',
+    },
+  },
+});
+
 
 export default function App() {
-  return  <div className="main">
+  return  <div className="wrapper">
+           <div className="titulo">
+            <CssVarsProvider theme={titletheme}>
+            <TitleHeader />
+            </CssVarsProvider>
+           </div>
+           <div className="main">
             <DropDown />
             <TableNumber />
           </div>
+         </div>
+}
+
+export function TitleHeader() {
+    return <Typography level="h1" align="center" sx={{ fontFamily: 'Serathine' }}
+              className = {titletheme.titleheader}
+           >Seating Chart</Typography>
 }
 
 export function DropDown() {
   return  <Autocomplete
             className = '_dropdown'
+            placeholder = "Enter your name"
             options={name_table_map}
             getOptionLabel={option => option.name}
             onChange={(event, value) =>
-                document.getElementById('_tableNumber').innerHTML= value?.id || 'Please select name'}
+                document.getElementById('_tableNumber').innerHTML= value?.id || ''}
           />;
 }
 
